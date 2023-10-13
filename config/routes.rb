c2d640043a1d07744aae6949831f05b6788332ff
Mucdat CENGIZ <mucdatcengiz@gmail.com>
2023-10-13 13:07:53 -0500

    auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/input', to: 'data#input'
   post '/generate', to: 'data#generate'
   get 'download_staff_csv', to: 'data#download_staff_csv'
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   get 'download_classes_csv', to: 'data#download_classes_csv'
   get 'download_enrollment_csv', to: 'data#download_enrollment_csv'
   root 'data#input'
+
 end
